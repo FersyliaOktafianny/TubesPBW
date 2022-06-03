35d9b30e2adbdbe12b2db0cbadb30215588b8816
Firman Sandy Prayitno <31910331+firmansandyp@users.noreply.github.com>
2022-06-03 14:27:03 +0700

    Memperbaiki index.js

--- SqlManager.js
+++ SqlManager.js
@@ -1,46 +1,47 @@
 import mysql from "mysql";
 
 export class SqlManager {
-  constructor() {
-    this.sqlPool = mysql.createPool({
-      user: "root",
-      password: "",
-      database: "ide",
-      host: "localhost",
-    });
-  }
-  //Function: Membuat koneksi database baru di dalam pool.
-  getDbConnection = () => {
-    return new Promise((resolve, reject) => {
-      this.sqlPool.getConnection((err, conn) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(conn);
-        }
-      });
-    });
-  };
-  //queryArguments harus berbentuk array.
-  executeQuery = (dbConn, query, queryArguments) => {
-    return new Promise((resolve, reject) => {
-      if (query.includes("?") && queryArguments.length > 0) {
-        dbConn.query(query, queryArguments, (err, result) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(result);
-          }
+    constructor() {
+        this.sqlPool = mysql.createPool({
+            user: "root",
+            password: "",
+            database: "ide",
+            host: "localhost",
         });
-      } else {
-        dbConn.query(query, (err, result) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(result);
-          }
+    }
+    //Function: Membuat koneksi database baru di dalam pool.
+    getDbConnection = () => {
+        return new Promise((resolve, reject) => {
+            this.sqlPool.getConnection((err, conn) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(conn);
+                }
+            });
         });
-      }
-    });
-  };
+    };
+    //queryArguments harus berbentuk array (bisa array kosong).
+    executeQuery = (query, queryArguments) => {
+        return new Promise((resolve, reject) => {
+            const dbConn = this.getDbConnection();
+            if (query.includes("?") && queryArguments.length > 0) {
+                dbConn.query(query, queryArguments, (err, result) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                    }
+                });
+            } else {
+                dbConn.query(query, (err, result) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                    }
+                });
+            }
+        });
+    };
 }
--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 import path from "path";
-
 import express from "express";
 import session from "express-session";
 
-import {SqlManager} from './SqlManager.js';
-
 const app = express();
 
-app.set("view engine", "ejs");
+//VIEW ENGINE
+//app.set("view engine", "ejs");
 
-const publicPath = path.resolve("public");
-app.use(express.static(publicPath));
-
+//FORM (POST) HANDLER
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+    express.urlencoded({
+        extended: true,
+    })
 );
 
+//[SEMENTARA] STATIC PATH
+const viewPath = path.resolve("view");
+const stylePath = path.resolve("style");
+const assetsPath = path.resolve("assets");
+app.use("/view", express.static(viewPath));
+app.use("/style", express.static(stylePath));
+app.use("/assets", express.static(assetsPath));
+
+//SESSION
 app.use(
-  session({
-    secret: "secret reddut",
-    name: "reddut.session",
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24, //1 hari
-    },
-  })
+    session({
+        secret: "secret rebbit",
+        name: "rebbit.session",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24, //1 hari
+        },
+    })
 );
 
-const sqlManager = new SqlManager();
+//ROUTE
+import { router as authRoute } from "./routes/auth.js";
+app.use("/auth", authRoute);
 
+//START
 const port = 8080;
-app.listen(port, () => {
-  console.log(".: Listening to port 8080");
+app.listen(port, (error) => {
+    if (error) {
+        console.log(".: ERROR");
+    } else {
+        console.log(".: Listening to port 8080");
+    }
 });
