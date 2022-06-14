c6fff04f9194b5a1d5081bc53e0cd0bbc1babd40
Firman Sandy Prayitno <31910331+firmansandyp@users.noreply.github.com>
2022-06-14 19:01:51 +0700

    Update models
    - sqlmanager.js

--- models/SqlManager.js
+++ models/SqlManager.js
@@ -1,47 +1,48 @@
 import mysql from "mysql";
 
-export class SqlManager {
-    constructor() {
-        this.sqlPool = mysql.createPool({
-            user: "root",
-            password: "",
-            database: "ide",
-            host: "localhost",
-        });
-    }
-    //Function: Membuat koneksi database baru di dalam pool.
-    getDbConnection = () => {
-        return new Promise((resolve, reject) => {
-            this.sqlPool.getConnection((err, conn) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(conn);
-                }
-            });
-        });
-    };
-    //queryArguments harus berbentuk array (bisa array kosong).
-    executeQuery = (query, queryArguments) => {
-        return new Promise((resolve, reject) => {
-            const dbConn = this.getDbConnection();
-            if (query.includes("?") && queryArguments.length > 0) {
-                dbConn.query(query, queryArguments, (err, result) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(result);
-                    }
-                });
-            } else {
-                dbConn.query(query, (err, result) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(result);
-                    }
-                });
-            }
-        });
-    };
+class sqlmanager {
+	constructor() {
+		this.sqlPool = mysql.createPool({
+			user: "root",
+			password: "",
+			database: "ide",
+			host: "localhost",
+		});
+	}
+	//Function: Membuat koneksi database baru di dalam pool.
+	getDbConnection = () => {
+		return new Promise((resolve, reject) => {
+			this.sqlPool.getConnection((err, conn) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(conn);
+				}
+			});
+		});
+	};
+	//queryArguments harus berbentuk array (bisa array kosong).
+	executeQuery = (query, queryArguments) => {
+		return new Promise((resolve, reject) => {
+			const dbConn = this.getDbConnection();
+			if (query.includes("?") && queryArguments.length > 0) {
+				dbConn.query(query, queryArguments, (err, result) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(result);
+					}
+				});
+			} else {
+				dbConn.query(query, (err, result) => {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(result);
+					}
+				});
+			}
+		});
+	};
 }
+export { sqlmanager };
