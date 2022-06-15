bbd66f72664414b664d6f59866cdc2b76670066f
amefedora <ginting.afi21@gmail.com>
2022-06-15 20:18:30 +0700

    test db

--- index.js
+++ index.js
@@ -15,34 +15,74 @@ app.use("/assets", express.static(assetsPath));
 
 //FORM (POST) HANDLER
 app.use(
-	express.urlencoded({
-		extended: true,
-	})
+    express.urlencoded({
+        extended: true,
+    })
 );
 
 //SESSION
 import session from "express-session";
 app.use(
-	session({
-		secret: "secret rebbit",
-		name: "rebbit.session",
-		resave: false,
-		saveUninitialized: false,
-		cookie: {
-			maxAge: 1000 * 60 * 60 * 24, //1 hari
-		},
-	})
+    session({
+        secret: "secret rebbit",
+        name: "rebbit.session",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 1000 * 60 * 60 * 24, //1 hari
+        },
+    })
 );
 
 //MYSQL
 import mysql from "mysql";
 import { getDbConnection, executeQuery } from "./models/sql.js";
 const sqlPool = mysql.createPool({
-	user: "root",
-	password: "",
-	database: "rebbit",
-	host: "localhost",
-});
+    user: "root",
+    password: "",
+    database: "rebbit",
+    host: "localhost",
+})
+
+//get all USERS
+app.get('', (req, res) => {
+    sqlPool.getConnection((err, connection) => {
+        if (err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+        connection.query('SELECT * FROM users', (err, rows) => {
+            connection.release()
+
+            if (!err) {
+                res.send(rows)
+            }
+            else {
+                console.log(err)
+            }
+        })
+    })
+})
+
+// add users
+app.post('/signup', (req, res) => {
+    sqlPool.getConnection((err, connection) => {
+        if (err) throw err
+        console.log(`connected as id ${connection.threadId}`)
+
+        const params = req.body
+
+        connection.query('INSERT INTO users SET ?', params, (err, rows) => {
+            connection.release()
+
+            if (!err) {
+                res.send(rows)
+            }
+            else {
+                console.log(err)
+            }
+        })
+    })
+})
 
 //ROUTE
 import { router as authRoute } from "./routes/loginsignup.js";
@@ -51,9 +91,9 @@ app.use("/auth", authRoute);
 //START
 const port = 8080;
 app.listen(port, (error) => {
-	if (error) {
-		console.log(".: ERROR");
-	} else {
-		console.log(".: Listening to port 8080");
-	}
+    if (error) {
+        console.log(".: ERROR");
+    } else {
+        console.log(".: Listening to port 8080");
+    }
 });
