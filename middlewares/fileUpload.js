d11e206002386d1f30eaa6ea08b14d68f340913c
Fersylia Oktafianny <6181901010@Student.unpar.ac.id>
2022-06-17 09:02:22 +0700

    Update fileUpload.js
    hapus direct path

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -6,7 +6,7 @@ http.createServer(function (req, res){
 const form = new formidable.IncomingForm();
     form.parse(req, function(error, fields, file){
         const filepath = file.fileupload.filepath;
-        const newpath ='C:\Users\viraa\Documents\BAHAN KULIAH SEMESTER 6\PBW\TubesPBW\assets';
+        const newpath ='';
         newpath += file.fileupload.originalFilename;
         fs.rename(filepath,newpath, function(){
             res.write('File Upload sukses');
