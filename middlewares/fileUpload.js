adef2c98464d3320dcb78d0ab9445b6df4334eb5
Fersylia Oktafianny <6181901010@Student.unpar.ac.id>
2022-06-17 08:45:44 +0700

    Update fileUpload.js
    coba dulu

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -4,6 +4,17 @@ import formidable from 'formidable'
 
 http.createServer(function (req, res){
 const form = new formidable.IncomingForm();
+    form.parse(req, function(error, fields, file){
+        const filepath = file.fileupload.filepath;
+        const newpath ='C:\Users\viraa\Documents\BAHAN KULIAH SEMESTER 6\PBW\TubesPBW\assets';
+        newpath += file.fileupload.originalFilename;
+        fs.rename(filepath,newpath, function(){
+            res.write('File Upload sukses');
+            res.end();
+        });
 
-})
+    }); 
+
+});
+
 
