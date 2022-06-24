048ec325a8efacf0a3e5cbbbfc6e5bc391eca1d3
Fersylia Oktafianny <6181901010@Student.unpar.ac.id>
2022-06-24 23:06:32 +0700

    file upload edited

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -1,28 +1,29 @@
 import http from 'http';
-import fs from 'fs'
+import fs, { appendFile } from 'fs'
 import formidable from 'formidable'
+import multer from "multer"; 
+import path from "path"
 
-http.createServer(function (req, res){
-const form = new formidable.IncomingForm();
-    form.parse(req, function(error, fields, file){
-        const filepath = file.fileupload.filepath;
-        const newpath ='';
-        newpath += file.fileupload.originalFilename;
-        fs.rename(filepath,newpath, function(){
-            res.write('File Upload sukses');
-            res.end();
-        });
-    }); 
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, "images");
+    },
+    filename: (req, file, cb)=>{
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
 });
 
+const upload = multer({storage: storage})
 
-import multer from "multer";
-const uploadFile = async (request, response, next) => {
-    const destination = (request, response, cb) =>{
-        cb(null, 'uploads');
-    };
-    const filename = (request, response, cb) =>{
-        cb(null, file.fieldname + '-' + Date.now());
-    };
-};
+
+
+// const uploadFile = async (request, response, next) => {
+//     const destination = (request, response, cb) =>{
+//         cb(null, 'uploads');
+//     };
+//     const filename = (request, response, cb) =>{
+//         cb(null, file.fieldname + '-' + Date.now());
+//     };
+// };
 
