b2483fdfdcc8db5910f2fd44738588055fcfcb4e
amefedora <ginting.afi21@gmail.com>
2022-06-29 09:18:31 +0700

    admin table

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -11,7 +11,7 @@ const getAllUsers = async (request, response, next) => {
 };
 
 const getAllThread = async (request, response, next) => {
-	const query = "SELECT * FROM threads ORDER BY created_date DESC;";
+	const query = "SELECT threads.*, thread_categories.name AS category, users.*, thread_contents.like_count FROM threads INNER JOIN thread_contents ON threads.id=thread_contents.thread_id INNER JOIN users ON thread_contents.author_id=users.id INNER JOIN thread_categories ON thread_categories.id = threads.category_id ORDER BY like_count DESC;";
 	const queryArgs = [];
 	const dbConn = await getDbConnection(sqlPool);
 	const result = await executeQuery(dbConn, query, queryArgs);
@@ -20,23 +20,5 @@ const getAllThread = async (request, response, next) => {
 	request.queryAllThread = result;
 	next();
 };
-
-// const getAllThread = async (request, response, next) => {
-// 	const threadid = request.params.threadid;
-// 	const query1 = "SELECT title FROM threads WHERE id=?;";
-// 	const queryArgs1 = [threadid];
-// 	const query2 = "SELECT * FROM threads INNER JOIN thread_contents ON threads.id=thread_contents.thread_id INNER JOIN users ON thread_contents.author_id=users.id WHERE thread_id=? ORDER BY thread_contents.like_count ASC";
-// 	const queryArgs2 = [threadid];
-// 	const dbConn = await getDbConnection(sqlPool);
-// 	const result1 = await executeQuery(dbConn, query1, queryArgs1);
-// 	const result2 = await executeQuery(dbConn, query2, queryArgs2);
-// 	dbConn.release();
-// 	console.log(result1);
-// 	console.log('==');
-// 	console.log(result2);
-// 	request.threadtitle = result1[0].title;
-// 	request.queryAllThisThreadContent = result2;
-// 	next();
-// };
 
 export {getAllUsers, getAllThread}
